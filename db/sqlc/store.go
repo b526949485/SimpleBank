f9d1d5649d285f7e02503850edad963cf95cf996
b526949485 <a526949485@gmail.com>
2024-05-31 18:09:25 +0800

    L6|7: A clean way to implement database transaction in Golang, DB transaction lock & How to handle deadlock in Golang

---
+++ db/sqlc/store.go
@@ -0,0 +1,19 @@
+package db
+
+import (
+	"database/sql"
+)
+
+// Store provides all functions to execute SQL queries and transactions
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+// NewStore creates a new store
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
